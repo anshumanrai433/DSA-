0ae0f8c81149256fffe2daf451b97d9814248074
Anshuman Rai <anshuman.23scse1012217@galgotiasuniversity.ac.in>
2025-12-12 23:09:45 +0530

    169_Majority_elements.java

---
+++ Leetcode/Easy/169_Majority_elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = 0, count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (candidate == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
